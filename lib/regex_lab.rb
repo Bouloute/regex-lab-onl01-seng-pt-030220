f0a8e4581c83e15f714789a93ef88a65ab53c348
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-10-21 21:44:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,10 +11,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  puts('Is ' + text + ' capitalized and end with punctuation?')
   text.match(/\A[A-Z].*[^a-zA-Z]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
+  puts("My phone number is " + phone)
   phone.match(/\d{10}/) ? true : false
 end
